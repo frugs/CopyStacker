96f43c18c1740a1dff55de8c7dca5f70d3930bd8
Hugo Wainwright <wainwrighthugo@gmail.com>
2015-06-04 13:53:08 +0100

    initial commit!

---
+++ CopyStacker/Main.fs
@@ -0,0 +1,14 @@
+﻿open System
+open System.Windows.Forms
+
+open CopyStacker
+open CopyStacker.Clipboard
+open CopyStacker.Windows
+
+[<EntryPoint>]
+let main argv = 
+    use trayIcon = new TrayIcon()
+    use form = new CopyStackerForm()
+
+    Application.Run()
+    0
---
+++ CopyStacker/TrayIcon.fs
@@ -0,0 +1,16 @@
+﻿namespace CopyStacker
+namespace CopyStacker
+
+open System.Drawing
+open System.Windows.Forms
+
+type TrayIcon() = 
+    let notifyIcon = new NotifyIcon()
+    do notifyIcon.Icon <- new Icon("icon.ico")
+    do notifyIcon.Text <- "CopyStacker"
+    do notifyIcon.Visible <- true
+    do notifyIcon.MouseClick.Add(fun mouseEventArgs -> do Application.Exit())
+
+    interface System.IDisposable with
+        member disposable.Dispose() =
+            notifyIcon.Dispose()
